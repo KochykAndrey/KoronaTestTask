547b20316feb172c434f4179520aa8d68ad1af9b
Andrey Kochyk <kochyk1999@gmail.com>
2024-03-21 17:27:30 +0300

    Initial commit

---
+++ src/main/java/by/kochyk/KoronaTestTask/KoronaTestTaskApplication.java
@@ -0,0 +1,13 @@
+package by.kochyk.KoronaTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoronaTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoronaTestTaskApplication.class, args);
+	}
+
+}
